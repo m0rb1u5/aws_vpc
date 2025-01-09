3ac431add023d93b573b4182e4ff8843ab0d8a16
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2025-01-09 14:45:49 -0300

    33. Criação de VPC na AWS com Remote State

---
+++ network.tf
@@ -0,0 +1,70 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc-terraform"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+
+  tags = {
+    Name = "subnet-terraform"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "internet-gateway-terraform"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "route-table-terraform"
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_security_group" "security_group" {
+  name        = "security-group-terraform"
+  description = "Permitir acesso na porta 22"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name = "security-group-terraform"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_ssh" {
+  description       = "SSH"
+  security_group_id = aws_security_group.security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress_rule" {
+  security_group_id = aws_security_group.security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  from_port         = 0
+  to_port           = 0
+}
